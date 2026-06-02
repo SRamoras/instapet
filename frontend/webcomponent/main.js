47f1f1531a3c53e5b3c05e227186d7fe4f78f60a
SRamoras <120826220+SRamoras@users.noreply.github.com>
2026-06-02 21:00:35 +0100

    feat: add webcomponents: bottom and layout components

---
+++ frontend/webcomponent/main.js
@@ -0,0 +1,5 @@
+import './ui/button.js';
+import './ui/postcard.js';
+import './layout/navbar.js';
+import './ui/usercard.js';
+
--- frontend/webcomponent/ui/main.js
+++
@@ -1,4 +0,0 @@
-import './button.js';
-import './postcard.js';
-import './navbar.js';
-import './usercard.js';
